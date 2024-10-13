dcec74fffd238e2478f835fda926ea42d43a1403
roy-corentin <corentin.roy@epitech.eu>
2024-10-13 22:34:33 +0200

    [ADD] method to convert i32 tu []u8 to display fps on screen

--- src/main.zig
+++ src/main.zig
@@ -1,19 +1,48 @@
+const std = @import("std");
 const rl = @import("raylib");
 
 pub fn main() !void {
     const screenWidth = 800;
     const screenHeight = 450;
+    const allocator = std.heap.c_allocator;
+
+    rl.setTargetFPS(60);
+    rl.setConfigFlags(rl.ConfigFlags{ .window_resizable = true });
 
     rl.initWindow(screenWidth, screenHeight, "ProceduralAnimation");
     defer rl.closeWindow();
 
-    rl.setTargetFPS(60);
-
     while (!rl.windowShouldClose()) {
         rl.beginDrawing();
         defer rl.endDrawing();
+        const fps: [:0]const u8 = try i32ToString(rl.getFPS());
+        defer allocator.free(fps);
 
         rl.clearBackground(rl.Color.white);
-        rl.drawText("Animation", 190, 200, 12, rl.Color.light_gray);
+        rl.drawText(fps, @divTrunc(rl.getScreenWidth(), 2), @divTrunc(rl.getScreenHeight(), 2), 24, rl.Color.red);
     }
+}
+
+pub fn i32ToString(number: i32) ![:0]const u8 {
+    var modulo: i32 = 1;
+    var size: u8 = 1;
+    var tmp = number;
+    const allocator = std.heap.c_allocator;
+
+    while (@divFloor(number, modulo) > 10) {
+        modulo *= 10;
+        size += 1;
+    }
+
+    var string: [:0]u8 = try allocator.allocSentinel(u8, size, 0);
+
+    while (size > 0) {
+        string[string.len - size] = @intCast(@divFloor(tmp, modulo) + 48);
+        size -= 1;
+        tmp = @mod(tmp, modulo);
+        modulo = @divFloor(modulo, 10);
+    }
+
+    std.debug.print("String: {s}, FPS: {d}\n", .{ string, number });
+    return string;
 }
