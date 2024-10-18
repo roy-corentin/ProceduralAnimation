8dd071e57835d5f9f9456360534a306df8918c39
roy-corentin <corentin.roy@epitech.eu>
2024-10-18 15:08:19 +0200

    [UP] extract some methods

--- src/main.zig
+++ src/main.zig
@@ -15,22 +15,28 @@ pub fn main() !void {
     while (!rl.windowShouldClose()) {
         rl.beginDrawing();
         rl.clearBackground(rl.Color.white);
-        rl.drawCircleLinesV(circlePosition, 30, rl.Color.red);
         rl.drawFPS(10, 10);
-        updateCirclePosition(&circlePosition);
+        drawCircle(circlePosition.*);
+        deplaceCircleInDirectionOfMouse(&circlePosition);
         rl.endDrawing();
     }
 }
 
-pub fn updateCirclePosition(circlePosition: *rl.Vector2) void {
-    const mousePosition = rl.getMousePosition();
-    const directionVector = computeDirection(circlePosition.*, mousePosition);
-    const speed = 5;
-    circlePosition.x += directionVector.x * speed;
-    circlePosition.y += directionVector.y * speed;
+pub fn drawCircle(circlePosition: rl.Vector2) void {
+    rl.drawCircleLinesV(circlePosition, 30, rl.Color.red);
 }
 
-pub fn computeDirection(pointA: rl.Vector2, pointB: rl.Vector2) rl.Vector2 {
+pub fn deplaceCircleInDirectionOfMouse(circlePosition: *rl.Vector2) void {
+    const mousePosition = rl.getMousePosition();
+
+    if (distanceBetween(circlePosition.*, mousePosition) < 5)
+        return;
+
+    const directionVector = computeDirectionVector(circlePosition.*, mousePosition);
+    updateCirclePosition(circlePosition, directionVector);
+}
+
+pub fn computeDirectionVector(pointA: rl.Vector2, pointB: rl.Vector2) rl.Vector2 {
     const displacementVector = .{ .x = pointB.x - pointA.x, .y = pointB.y - pointA.y };
     const magnitudeDeplacementVector = absoluteValue(displacementVector.x) + absoluteValue(displacementVector.y);
 
@@ -39,4 +45,15 @@ pub fn computeDirection(pointA: rl.Vector2, pointB: rl.Vector2) rl.Vector2 {
 
 pub fn absoluteValue(number: f32) f32 {
     return if (number < 0) number * -1 else number;
+}
+
+pub fn distanceBetween(pointA: rl.Vector2, pointB: rl.Vector2) f32 {
+    return absoluteValue(pointB.x - pointA.x) + absoluteValue(pointB.y - pointA.y);
+}
+
+pub fn updateCirclePosition(circlePosition: *rl.Vector2, directionVector: rl.Vector2) void {
+    const speed = 5;
+
+    circlePosition.x += directionVector.x * speed;
+    circlePosition.y += directionVector.y * speed;
 }
